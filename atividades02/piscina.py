928132663e3c6152ae76e5293d67783b98aafc31
Lucas Santos <98106403+bronzeline@users.noreply.github.com>
2023-05-23 11:47:33 -0300

    piscina.py

---
+++ atividades02/piscina.py
@@ -0,0 +1,5 @@
+lado1 = float(input("Insira a medida de um dos lados: "))
+lado2= float(input("Insira a medidade do outro lado: "))
+profun = float(input("Insira a profundidade: "))
+volume = lado1 * lado2 * profun
+print("O volume da piscina é de: ",volume)
