9b8e18cf89064f136abe84126159251a9af678ad
Lucas Santos <98106403+bronzeline@users.noreply.github.com>
2023-05-23 08:10:16 -0300

    nonascimento.py

---
+++ Atividades/anonascimento.py
@@ -0,0 +1,6 @@
+ano = int(input("Digite seu ano de nascimento: "))
+anoatual= int(input("Digite o ano atual: "))
+idade = anoatual - ano
+idadeano = 2099 - ano
+print ("Você tem: ", idade, "anos" )
+print ("Em 2099 você terá:", idadeano, "anos")
