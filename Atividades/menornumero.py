19ec17b5762dc792ea3bc55da708be80f0355afa
Lucas Santos <98106403+bronzeline@users.noreply.github.com>
2023-05-23 09:37:57 -0300

    menornumero.py

---
+++ Atividades/menornumero.py
@@ -0,0 +1,17 @@
+numero = int(input("Insira um número de 1 a 7; Para saber o dia da semana: "))
+if numero == 1:
+    print("Domingo")
+elif numero == 2:
+    print("Segunda-Feira")
+elif numero == 3:
+    print("Terça-Feira")
+elif numero == 4:
+    print("Quarta-Feira")
+elif numero == 5:
+    print("Quinta-Feira")
+elif numero == 6:
+    print("Sexta-Feira")
+elif numero == 7:
+    print("Sábado")
+else:
+    print("Valor Inválido")
