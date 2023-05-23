246fe46687cca7f627e8ecb29fab295eda66df70
Lucas Santos <98106403+bronzeline@users.noreply.github.com>
2023-05-23 10:01:13 -0300

    estação.py

---
+++ Atividades/estação.py
@@ -0,0 +1,27 @@
+num = int(input("Insira o mês(número): "))
+if num == 1:
+    print("Verão")
+elif num == 2:
+    print("Verão")
+elif num == 3:
+    print("verão")
+elif num == 4:
+    print("Outono")
+elif num == 5:
+    print("Outono")
+elif num == 6:
+    print("Outono")
+elif num == 7:
+    print("Inverno")
+elif num == 8:
+    print("Inverno")
+elif num == 9:
+    print("Inverno")
+elif num == 10:
+    print("Primavera")
+elif num == 11:
+    print("Primavera")
+elif num == 12:
+    print("Primavera")
+else:
+    print("Número Inválido")
