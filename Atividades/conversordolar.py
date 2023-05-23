ac6cd7d92d825209242cfb3cda174d3fe4f57722
Lucas Santos <98106403+bronzeline@users.noreply.github.com>
2023-05-23 08:15:07 -0300

    conversordolar.py

---
+++ Atividades/conversordolar.py
@@ -0,0 +1,3 @@
+dolar = float(input("Insira o valor desejado para sua conversão em real(R$): U$"))
+real = dolar / 5.01
+print("A quantia de U${:.2f} convertida para real é R${:.2f}".format(dolar,real))
