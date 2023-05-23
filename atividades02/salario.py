9be3e341b5ebd23ef6762f9ef7ab509bb83cd4a3
Lucas Santos <98106403+bronzeline@users.noreply.github.com>
2023-05-23 11:31:23 -0300

    salario.py

--- atividades02/salario.py
+++ atividades02/salario.py
@@ -4,6 +4,7 @@ if n > 50:
     e = (n-50) * 20
     total = 50 * 10 + e
 else:
-    n * 10
+    total= n * 10
+    e = 0
 print("O salário total é de R$: ", total)
 print("O salário excedente é de R$: ", e)
