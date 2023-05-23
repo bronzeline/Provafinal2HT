92673cbd7551a1722522e4f46c67381e20460b36
Lucas Santos <98106403+bronzeline@users.noreply.github.com>
2023-05-23 10:56:39 -0300

    produtocor.py

---
+++ atividades02/produtocor.py
@@ -0,0 +1,12 @@
+print("Produtos: Verde/Azul/Amarelo/Vermelho")
+cor = str(input("Escolha a cor do produto: "))
+if cor == "vermelho":
+    print("Preço do produto vermelho: R$10,00")
+elif cor == "azul":
+    print("Preço do produto azul: R$20,00")
+elif cor == "amarelo":
+    print("Preço do produto amarelo: R$30,00")
+elif cor == "verde":
+    print("Preço do produto amarelo: R$40,00")
+else:
+    print("Cor Inválida")
