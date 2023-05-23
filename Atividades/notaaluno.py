3c6a57b32d1d2d7c6d1ecc358c0eb87815eb9a74
Lucas Santos <98106403+bronzeline@users.noreply.github.com>
2023-05-23 09:52:23 -0300

    notaaluno.py

---
+++ Atividades/notaaluno.py
@@ -0,0 +1,11 @@
+nota = float(input("Insira sua nota: "))
+if nota == 10:
+    print("Excelente")
+elif nota >= 8:
+    print("Ótimo")
+elif nota >= 7:
+    print("Bom")
+elif nota >= 5:
+    print("Regular")
+else:
+    print("Insuficiente")
