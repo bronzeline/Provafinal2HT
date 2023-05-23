95af4118cd125c9fd29819f4183dee36d330123d
Lucas Santos <98106403+bronzeline@users.noreply.github.com>
2023-05-23 11:54:15 -0300

    consumomedioegasto.py

---
+++ atividades02/consumomedioegasto.py
@@ -0,0 +1,7 @@
+km = float(input("Insira quantos km foram percorridos: "))
+gasolina = float(input("Insira o valor gasto da gasolina: "))
+valorgasosa = float(input("Insira o preço da gasolina: "))
+consumo = km/gasolina
+gastos = valorgasosa * 1
+print("O consumo médio em km é de: ", consumo)
+print("O valor gasto é de: R$ ", gastos)
