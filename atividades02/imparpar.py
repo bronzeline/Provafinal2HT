89e202a8c9b2cf0c766b97f24d0cf67739e7837c
Lucas Santos <98106403+bronzeline@users.noreply.github.com>
2023-05-23 11:10:16 -0300

    imparpar.py

---
+++ atividades02/imparpar.py
@@ -0,0 +1,6 @@
+numero = int(input("Insira o número para verificar se é ímpar ou par: "))
+resultado = numero % 2
+if resultado == 0:
+    print("O número é par")
+else:
+    print("O número é ímpar")
